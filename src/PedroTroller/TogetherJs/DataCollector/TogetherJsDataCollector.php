17ecadbfcdc93ecdf40419b8a4a849be7f07bba4
Pierre PLAZANET <plazanet.pierre@gmail.com>
2014-10-27 14:37:07 +0100

    Initial commit

---
+++ src/PedroTroller/TogetherJs/DataCollector/TogetherJsDataCollector.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PedroTroller\TogetherJs\DataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface;
+
+class TogetherJsDataCollector implements DataCollectorInterface
+{
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+
+    }
+
+    public function getName()
+    {
+        return 'togetherjs';
+    }
+}
---
+++ src/PedroTroller/TogetherJs/PedroTrollerTogetherJsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PedroTroller\TogetherJs;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PedroTrollerTogetherJsBundle extends Bundle
+{
+}

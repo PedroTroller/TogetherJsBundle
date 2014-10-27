31d10a0ba43bac1442122a56b3ac1d7c11abf4ff
Pierre PLAZANET <plazanet.pierre@gmail.com>
2014-10-27 16:50:17 +0100

    Don't feed the troll

--- src/PedroTroller/TogetherJs/DataCollector/TogetherJsDataCollector.php
+++ src/PedroTroller/TogetherJs/DataCollector/TogetherJsDataCollector.php
@@ -8,7 +8,9 @@ use Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface;
 
 class TogetherJsDataCollector implements DataCollectorInterface
 {
-    public function collect(Request $request, Response $response, \Exception $exception = null) {}
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+    }
 
     public function getName()
     {
